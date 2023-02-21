73b1ee451ae87426d7d4f52ca970e01668d2b3c8
Sadula Rushidhar <115465446+S-Rushidhar-1-1999@users.noreply.github.com>
2023-02-21 23:48:47 +0530

    Update start.py

--- WebStreamer/bot/plugins/start.py
+++ WebStreamer/bot/plugins/start.py
@@ -1,14 +1,14 @@
 # This file is a part of TG-FileStreamBot
 # Coding : Jyothis Jayanth [@EverythingSuckz]
 
-from pyrogram import filters
+from pyrogram import filters, Client
 from pyrogram.types import Message
 from pyrogram.errors import UserNotParticipant
 from WebStreamer.vars import Var 
 from WebStreamer.bot import StreamBot
 
 @StreamBot.on_message(filters.command(["start", "help"]) & filters.private)
-async def start(b: bot, m: Message):
+async def start(b: Client, m: Message):
     if Var.ALLOWED_USERS and not ((str(m.from_user.id) in Var.ALLOWED_USERS) or (m.from_user.username in Var.ALLOWED_USERS)):
         return await m.reply(
             "You are not in the allowed list of users who can use me. \
